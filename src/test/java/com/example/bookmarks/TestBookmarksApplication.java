e41a5340f58099c8a1bc29e6a22b4a5fd37001c3
Simon Vargas <sjvargas01@gmail.com>
2024-09-26 23:42:30 -0400

    first commit

---
+++ src/test/java/com/example/bookmarks/TestBookmarksApplication.java
@@ -0,0 +1,11 @@
+package com.example.bookmarks;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BookmarksApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
